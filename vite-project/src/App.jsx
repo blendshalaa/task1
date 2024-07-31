4cf1f89053c13e86542f7fd64e348c6076980995
blendshalaa <90574525+blendshalaa@users.noreply.github.com>
2024-07-31 16:32:45 +0200

    fix

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,13 +6,20 @@ function App() {
 
   const items=[
     {title:'The TLD',subtitle:'Does the domain extension match the language of the domain name',icon:'TLD'},
-    {title:'International recognition',subtitle:'Can the domain name be us on an international scale',icon:'Recognition'},
-    {title:'Sales Opportunities',subtitle:'Can the domain name be used on an internation scale',icon:'Sales'},
     {title:'Domain Length',subtitle:'Is the domain short enough to remember',icon:'Ruler'},
-    {title:'Search engine',subtitle:'Does the domain follow search engine guidelines',icon:'Search'},
-    {title:'Typo susceptibility',subtitle:'How high is the risk of mistyping the domain name',icon:'T'},
+   
     {title:'Language',subtitle:'How complex is the actual domain name',icon:'Language'},
+    
+   
+    {title:'International recognition',subtitle:'Can the domain name be us on an international scale',icon:'Recognition'},
+    {title:'Search engine',subtitle:'Does the domain follow search engine guidelines',icon:'Search'},
+   
     {title:'Advertising Potential',subtitle:'Could the domain be used for advertising campaings',icon:'Advert'},
+  
+  
+    {title:'Sales Opportunities',subtitle:'Can the domain name be used on an internation scale',icon:'Sales'},
+    {title:'Typo susceptibility',subtitle:'How high is the risk of mistyping the domain name',icon:'T'},
+  
     {title:'Business potential',subtitle:'Can the domain be used as your company adress',icon:'Business'},
 
   ]
